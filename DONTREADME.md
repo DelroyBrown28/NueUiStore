512b8b54b792c1c40d03c7e46d1cd9429a6ec7d2
Delroy Brown <delroybrown.db@gmail.com>
2021-04-07 17:51:53 +0100

    Home page set up and navbar almost complete

---
+++ DONTREADME.md
@@ -0,0 +1,2 @@
+1. Navbar displaying links
+2. featured gallery on home page
---
+++ NueUiStore/home/models.py
@@ -0,0 +1,108 @@
+from django.db import models
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import (FieldPanel, PageChooserPanel,
+                                            MultiFieldPanel,
+                                            InlinePanel,
+                                            FieldRowPanel)
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+
+class HomePage(Page):
+    templates = "home/home_page.html"
+    max_count = 1
+
+    page_title = models.CharField(max_length=100, blank=False, null=True)
+    page_subtitle = RichTextField(default='Page Subtitle', blank=True, null=True, features=["bold", "italic"])
+    page_description = RichTextField(default='Place description here', null=True, blank=True)
+    home_page_background_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    home_page_CTA_button = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+   )
+    home_page_cta_button_label = models.CharField(max_length=25, blank=False, null=False)
+
+    """Navbar Stuff."""
+    navbar_logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    navbar_link_1 = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    navbar_link_text_1 = models.CharField(max_length=25, help_text='Navbar Text', blank=True, null=True, default='Nav Text 1')
+    navbar_link_2 = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    navbar_link_text_2 = models.CharField(max_length=25, help_text='Navbar Text', blank=True, null=True, default='Nav Text 2')
+    navbar_link_3 = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    navbar_link_text_3 = models.CharField(max_length=25, help_text='Navbar Text', blank=True, null=True, default='Nav Text 3')
+    navbar_link_4 = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    navbar_link_text_4 = models.CharField(max_length=25, help_text='Navbar Text', blank=True, null=True, default='Nav Text 4')
+    
+    content_panels = Page.content_panels + [
+        FieldPanel("page_title"),
+        FieldPanel("page_subtitle"),
+        FieldPanel("page_description"),
+        ImageChooserPanel("home_page_background_image"),
+        PageChooserPanel("home_page_CTA_button"),
+        FieldPanel("home_page_cta_button_label"),
+        # Navbar Stuff
+        ImageChooserPanel('navbar_logo'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('navbar_link_text_1', classname='col12'),
+                PageChooserPanel('navbar_link_1'),
+            ]),
+            FieldRowPanel([
+                FieldPanel('navbar_link_text_2', classname='col12'),
+                PageChooserPanel('navbar_link_2'),
+            ]),
+            FieldRowPanel([
+                FieldPanel('navbar_link_text_3', classname='col12'),
+                PageChooserPanel('navbar_link_3'),
+            ]),
+            FieldRowPanel([
+                FieldPanel('navbar_link_text_4', classname='col12'),
+                PageChooserPanel('navbar_link_4'),
+            ]),
+        ], heading='Navbar Links'),
+    ]
+
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
