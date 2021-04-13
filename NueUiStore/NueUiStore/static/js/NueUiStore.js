3e58f65359fdd6c7fa4de63260aac7f0d788e319
Delroy Brown <delroybrown.db@gmail.com>
2021-04-13 12:17:00 +0100

    Gsapped the frontend to show About page onClick

--- NueUiStore/NueUiStore/static/js/NueUiStore.js
+++ NueUiStore/NueUiStore/static/js/NueUiStore.js
@@ -1,4 +1,55 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-        
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+const aboutButton = document.querySelector('.nav-about-button');
+const closeAboutButton = document.querySelector('.about-page-close-btn');
+// const navOpen = document.querySelector('nav-open');
+
+
+// Stops animation from playing unless refreshed
+const tl = new TimelineLite({
+  paused: true,
+  reversed: true
+});
+
+tl.to('.art-cards, .cta-wrap', 0.3, {
+    autoAlpha: 0,
+    stagger: 0.1,
+    ease: Power2.easeOut,
+  })
+  .to('.artist-feature-section', 0.3, {
+    autoAlpha: 1,
+    ease: Power2.easeOut,
+  })
+  .to('.about-page-close-btn', 0.3, {
+    autoAlpha: 1,
+    ease: Power2.easeOut,
+  })
+  
+  
+
+// Button to toggle animation
+aboutButton.addEventListener('click', () => {
+
+  if (tl.isActive()) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    return false;
+  }
+  toggleTween(tl)
+})
+
+closeAboutButton.addEventListener('click', () => {
+
+  if (tl.isActive()) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    return false;
+  }
+  toggleTween(tl)
+})
+
+function toggleTween(tween) {
+  tween.reversed() ? tween.play() : tween.reverse();
+}
